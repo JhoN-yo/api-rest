0b8eb52d41d14f8b94e49a5ea8ea84ca9abeb3ad
JhoN.yo <jhon.yo@pop-os.localdomain>
2022-11-29 18:38:22 -0300

    feat: initial structure

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 import { server } from './server/Server';
 
-server.listen(3333, () => console.log('Server listening'));
+server.listen(process.env.PORT || 3333, () => console.log(`Server listening Port: ${process.env.PORT || 3333}`));
